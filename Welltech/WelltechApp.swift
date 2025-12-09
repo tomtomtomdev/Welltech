3a3003d463e388b2a2aa51ce953db81d95508792
tom <tommy.yohanes@icloud.com>
2025-12-09 21:38:58 +0700

    Initial Commit

---
+++ Welltech/WelltechApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelltechApp.swift
+//  Welltech
+//
+//  Created by tomtomtom on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WelltechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WelltechTests/WelltechTests.swift
@@ -0,0 +1,17 @@
+//
+//  WelltechTests.swift
+//  WelltechTests
+//
+//  Created by tomtomtom on 12/9/25.
+//
+
+import Testing
+@testable import Welltech
+
+struct WelltechTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
